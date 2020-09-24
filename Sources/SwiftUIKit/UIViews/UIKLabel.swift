a647b9afb3cae48604258a731c07a86c41bfc730
Seyed Mojtaba Hosseini Zeidabadi <chenzook@gmail.com>
2020-09-24 17:23:06 +0330

    UILabel added.

---
+++ Sources/SwiftUIKit/UIViews/UIKLabel.swift
@@ -0,0 +1,21 @@
+//
+//  UIKLabel.swift
+//  SwiftUIKit
+//
+//  Created by Seyed Mojtaba Hosseini Zeidabadi on 9/22/20.
+//  Copyright © 2020 Chenzook. All rights reserved.
+//
+//  StackOverflow: https://stackoverflow.com/story/mojtabahosseini
+//  Linkedin: https://linkedin.com/in/MojtabaHosseini
+//  GitHub: https://github.com/MojtabaHs
+//
+
+import UIKit
+
+public struct UIKLabel: ViewRepresentableHelper {
+    public var configuration = { (view: UILabel) in }
+
+    public init(configuration: @escaping (UIViewType) -> () = { _ in }) {
+        self.configuration = configuration
+    }
+}
