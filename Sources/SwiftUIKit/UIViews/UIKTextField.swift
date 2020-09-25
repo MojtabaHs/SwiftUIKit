1c9c0fa036a98c9a0191b42c98db220e8219a9fc
Seyed Mojtaba Hosseini Zeidabadi <chenzook@gmail.com>
2020-09-25 20:42:34 +0330

    Binding text added for the UIKTextField.

--- Sources/SwiftUIKit/UIViews/UIKTextField.swift
+++ Sources/SwiftUIKit/UIViews/UIKTextField.swift
@@ -10,12 +10,46 @@
 //  GitHub: https://github.com/MojtabaHs
 //
 
-import UIKit
+import SwiftUI
 
 public struct UIKTextField: ViewRepresentableHelper {
-    public var configuration = { (view: UITextField) in }
+    public typealias ViewType = UITextField
 
-    public init(configuration: @escaping (UIViewType) -> () = { _ in }) {
+    @Binding public var text: String
+
+    public var configuration = { (view: ViewType) in }
+
+    public init(text: Binding<String>, configuration: @escaping (ViewType) -> () = { _ in }) {
         self.configuration = configuration
+        self._text = text
+    }
+
+    public func makeUIView(context: Context) -> ViewType {
+        let view = makeView(context)
+        view.setContentHuggingPriority(.defaultLow, for: .horizontal)
+        view.delegate = context.coordinator
+        view.addTarget(context.coordinator, action: #selector(Coordinator.textFieldDidChange(_:)), for: .editingChanged)
+        return view
+    }
+
+    public func updateUIView(_ uiView: ViewType, context: Context) {
+        updateView(uiView, context)
+        uiView.text = text
+    }
+
+    public func makeCoordinator() -> Coordinator {
+        Coordinator($text)
+    }
+
+    public class Coordinator: NSObject, UITextFieldDelegate {
+        var text: Binding<String>
+
+        init(_ text: Binding<String>) {
+            self.text = text
+        }
+
+        @objc public func textFieldDidChange(_ textView: UITextField) {
+            self.text.wrappedValue = textView.text ?? ""
+        }
     }
 }
