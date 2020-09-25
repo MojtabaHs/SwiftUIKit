bb2496be2f1573ac19518f09f6ecd01f3cfac2a7
Seyed Mojtaba Hosseini Zeidabadi <chenzook@gmail.com>
2020-09-25 20:42:41 +0330

    Binding text added for the UIKTextView.

--- Sources/SwiftUIKit/UIViews/UIKTextView.swift
+++ Sources/SwiftUIKit/UIViews/UIKTextView.swift
@@ -10,12 +10,45 @@
 //  GitHub: https://github.com/MojtabaHs
 //
 
-import UIKit
+import SwiftUI
 
 public struct UIKTextView: ViewRepresentableHelper {
-    public var configuration = { (view: UITextView) in }
+    public typealias ViewType = UITextView
 
-    public init(configuration: @escaping (UIViewType) -> () = { _ in }) {
+    @Binding public var text: String
+
+    public var configuration = { (view: ViewType) in }
+
+    public init(text: Binding<String>, configuration: @escaping (ViewType) -> () = { _ in }) {
         self.configuration = configuration
+        self._text = text
+    }
+
+    public func makeUIView(context: Context) -> ViewType {
+        let view = makeView(context)
+        view.setContentHuggingPriority(.defaultLow, for: .horizontal)
+        view.delegate = context.coordinator
+        return view
+    }
+
+    public func updateUIView(_ uiView: ViewType, context: Context) {
+        updateView(uiView, context)
+        uiView.text = text
+    }
+
+    public func makeCoordinator() -> Coordinator {
+        Coordinator($text)
+    }
+
+    public class Coordinator: NSObject, UITextViewDelegate {
+        var text: Binding<String>
+
+        init(_ text: Binding<String>) {
+            self.text = text
+        }
+
+        public func textViewDidChange(_ textView: ViewType) {
+            self.text.wrappedValue = textView.text
+        }
     }
 }
